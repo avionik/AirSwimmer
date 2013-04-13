24d29cb71b534927402f9e60918470c2ba1c773d
Caroline Pilot <ff0425@haw-ingolstadt>
2013-04-13 23:21:07 +0200

    Startseite + Tasten

--- Oberfläche/src/AirSwimmer/gen/de/example/airswimmer/R.java
+++ Oberfläche/src/AirSwimmer/gen/de/example/airswimmer/R.java
@@ -10,6 +10,13 @@ package de.example.airswimmer;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int Dark=0x7f040001;
+        public static final int Marineblau=0x7f040003;
+        public static final int Orange=0x7f040000;
+        public static final int Tuerkis=0x7f040002;
+        public static final int Weiss=0x7f040004;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,37 +24,50 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int down=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int left=0x7f020002;
-        public static final int right=0x7f020003;
-        public static final int up=0x7f020004;
+        public static final int airswimmer=0x7f020000;
+        public static final int down=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int left=0x7f020003;
+        public static final int right=0x7f020004;
+        public static final int up=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int button_down=0x7f080002;
-        public static final int button_left=0x7f080001;
-        public static final int button_right=0x7f080003;
-        public static final int button_up=0x7f080000;
+        public static final int Button_Kippen=0x7f090008;
+        public static final int Button_Tasten=0x7f090006;
+        public static final int Button_Wischen=0x7f090007;
+        public static final int RelativeLayout1=0x7f090004;
+        public static final int action_settings=0x7f09000a;
+        public static final int button_down=0x7f090002;
+        public static final int button_left=0x7f090001;
+        public static final int button_right=0x7f090003;
+        public static final int button_up=0x7f090000;
+        public static final int imageView1=0x7f090009;
+        public static final int linearLayout1=0x7f090005;
     }
     public static final class layout {
-        public static final int activity_start=0x7f030000;
+        public static final int activity_layout_tasten=0x7f030000;
+        public static final int front_page=0x7f030001;
     }
     public static final class menu {
-        public static final int start=0x7f070000;
+        public static final int activity__tasten=0x7f080000;
+        public static final int start=0x7f080001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int button_down=0x7f050004;
-        public static final int button_left=0x7f050006;
-        public static final int button_right=0x7f050005;
-        public static final int button_up=0x7f050003;
-        public static final int hello_world=0x7f050002;
+        public static final int Kippen=0x7f060009;
+        public static final int Tasten=0x7f060007;
+        public static final int Wischen=0x7f060008;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int button_down=0x7f060004;
+        public static final int button_left=0x7f060006;
+        public static final int button_right=0x7f060005;
+        public static final int button_up=0x7f060003;
+        public static final int hello_world=0x7f060002;
+        public static final int title_activity_activity__tasten=0x7f06000a;
     }
     public static final class style {
         /** 
@@ -71,10 +91,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
