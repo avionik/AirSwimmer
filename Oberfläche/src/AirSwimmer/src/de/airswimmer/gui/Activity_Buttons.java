aba6fbc7bee42d07d4a21792810f38841cf6cdfe
Caroline Pilot <ff0425@haw-ingolstadt>
2013-04-26 18:51:47 +0200

    Buttons reagieren, wenn heruntergedrückt wird V1

--- Oberfläche/src/AirSwimmer/src/de/airswimmer/gui/Activity_Buttons.java
+++ Oberfläche/src/AirSwimmer/src/de/airswimmer/gui/Activity_Buttons.java
@@ -1,12 +1,14 @@
 package de.airswimmer.gui;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class Activity_Buttons extends BaseActivity implements OnClickListener{
+public class Activity_Buttons extends BaseActivity implements OnClickListener {
 
 	private ImageButton button_up;
 	private ImageButton button_down;
@@ -28,21 +30,66 @@ public class Activity_Buttons extends BaseActivity implements OnClickListener{
 		button_left.setOnClickListener(this);
 	}
 
-
 	@Override
 	public void onClick(View v) { // what happens when you click a button
 		if (v == button_up) { // if button_up was clicked
-			//TODO command up 
-			Toast.makeText(v.getContext(), "up", Toast.LENGTH_SHORT).show(); // a text pops up for a short amount of time showing "up"
+			// TODO command up
+
+			button_up.setOnTouchListener(new OnTouchListener() {
+				public boolean onTouch(View v, MotionEvent event) {
+					if (event.getAction() == MotionEvent.ACTION_DOWN) { // action while pressing the button down
+						Toast.makeText(v.getContext(), "up", Toast.LENGTH_SHORT)
+								.show(); // a text pops up for a short amount of
+											// time showing "up"
+						return true;
+					}
+					return false;
+				}
+			});
+
 		} else if (v == button_down) {
-			//TODO command down
-			Toast.makeText(v.getContext(), "down", Toast.LENGTH_SHORT).show();
+			// TODO command down
+
+			button_down.setOnTouchListener(new OnTouchListener() {
+				public boolean onTouch(View v, MotionEvent event) {
+					if (event.getAction() == MotionEvent.ACTION_DOWN) {
+						Toast.makeText(v.getContext(), "down",
+								Toast.LENGTH_SHORT).show();
+						return true;
+					}
+					return false;
+				}
+			});
+
 		} else if (v == button_right) {
-			//TODO command right
-			Toast.makeText(v.getContext(), "right", Toast.LENGTH_SHORT).show();
+			// TODO command right
+
+			button_right.setOnTouchListener(new OnTouchListener() {
+				public boolean onTouch(View v, MotionEvent event) {
+					if (event.getAction() == MotionEvent.ACTION_DOWN) {
+						Toast.makeText(v.getContext(), "right",
+								Toast.LENGTH_SHORT).show();
+						return true;
+					}
+					return false;
+				}
+			});
+
 		} else if (v == button_left) {
-			//TODO command left
-			Toast.makeText(v.getContext(), "left", Toast.LENGTH_SHORT).show();
+			// TODO command left
+
+			button_left.setOnTouchListener(new OnTouchListener() {
+				public boolean onTouch(View v, MotionEvent event) {
+					if (event.getAction() == MotionEvent.ACTION_DOWN) {
+						Toast.makeText(v.getContext(), "left",
+								Toast.LENGTH_SHORT).show();
+						return true;
+					}
+					return false;
+				}
+			});
+
 		}
 	}
+
 }
