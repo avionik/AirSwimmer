08cab020485dd6622bc14c173b5cf1cc66b0de12
Sabine Kressierer <ff0863@haw-ingolstadt.de>
2013-05-04 20:38:13 +0200

    umbenannte Files hinzugefügt

---
+++ Oberfläche/src/AirSwimmer/src/de/airswimmer/gui/Activity_Wipe.java
@@ -0,0 +1,15 @@
+package de.airswimmer.gui;
+
+/**
+ * Class for wiping where one slide causes one move of airswimmer
+ * 
+ */
+
+public class Activity_Wipe extends BaseWipe {
+
+    // method for movement of the fish
+    public void move(int xAxis, int yAxis) {
+        // TODO: implement movement (one slide-->one move of tail fin)
+    }
+
+}
---
+++ Oberfläche/src/AirSwimmer/src/de/airswimmer/gui/Activity_Wipe_Permanent.java
@@ -0,0 +1,33 @@
+package de.airswimmer.gui;
+
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+
+
+/**
+ * Class for wiping where shark is swimming permanently and sliding causes change of direction
+ * 
+ */
+public class Activity_Wipe_Permanent extends BaseWipe {
+    boolean swimmForward = false; //indicates if AirSwimmer has to swim forward
+    @Override
+    protected boolean moveImage(View view, MotionEvent event) {
+        switch(event.getActionMasked()){ //checks if screen is touched so AirSwimmer has to swim
+        case MotionEvent.ACTION_UP:swimmForward=false;
+
+        break;
+        case MotionEvent.ACTION_DOWN:
+            swimmForward=true;     
+        }
+        return super.moveImage(view, event); // moves image of shark
+    }
+
+    
+    @Override
+    public void move(int xAxis, int yAxis) {
+        // TODO Implement movement: swimmForward=true -->AirSwimmer has to swim
+    }
+
+}
