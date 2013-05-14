746f57a4c316fe4961d888169b1f33f8e4fa6c22
Caroline Pilot <ff0425@haw-ingolstadt>
2013-05-14 21:06:15 +0200

    Android Action Bar angepasst

--- Oberfläche/src/AirSwimmer/src/de/airswimmer/gui/BaseActivity.java
+++ Oberfläche/src/AirSwimmer/src/de/airswimmer/gui/BaseActivity.java
@@ -189,7 +189,7 @@ public class BaseActivity extends Activity {
 				case R.id.permanent:
 					// * TODO --> name of new permanent tilt class
 					startActivity(new Intent(this,
-							Activity_Tilt_Permanent.class));
+							Start_button_tilt.class));
 					return true;
 				case R.id.single:
 					startActivity(new Intent(this, Activity_Tilt.class));
