460d005c63a877cfd37bcd19270961522db22747
Sabine Kressierer <ff0863@haw-ingolstadt.de>
2013-06-03 16:11:50 +0200

    nicht benötigte xml-Dateien in Menu Ordner gelöscht in BaseButtons.java: onClickListener in onTouchListener umgewandelt um ab erster Berührungzwischen drücken und loslassen zu unterscheiden Formatierung/Kommentare

--- Oberfläche/src/AirSwimmer/src/de/airswimmer/gui/BaseTilt.java
+++ Oberfläche/src/AirSwimmer/src/de/airswimmer/gui/BaseTilt.java
@@ -1,7 +1,5 @@
 package de.airswimmer.gui;
 
-
-
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -19,187 +17,199 @@ import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public abstract class BaseTilt extends BaseActivity implements SensorEventListener {
+public abstract class BaseTilt extends BaseActivity implements
+		SensorEventListener {
 
 	private Bitmap shark;
 	private SensorManager sm;
 	protected DrawShark ourView;
 	private float sensorX, sensorY;
 	private Bitmap mBitmap;
-	
+
 	@Override
-	public void onAccuracyChanged(Sensor arg0, int arg1) { 
-		
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+
 	}
 
 	@Override
-	public void onSensorChanged(SensorEvent event) { // to able to detect the change of the sensor 		
+	public void onSensorChanged(SensorEvent event) { // to able to detect the
+														// change of the sensor
 		sensorY = event.values[0];
 		sensorX = event.values[1];
-		
+
 	} // class to handle the sensor events
-	
-	 public class DrawShark extends SurfaceView implements Runnable{
 
-	        SurfaceHolder ourHolder;
-	        Thread ourThread = null;
-	        boolean isRunning = true;
-	        int background = R.drawable.ic_sky;
-	        
-	        public DrawShark(Context context) {
-	            super(context);
-	            ourHolder = getHolder();
-	        }
+	// thread to draw moving picture of shark
+	public class DrawShark extends SurfaceView implements Runnable {
 
-	        public void pause(){
-	        	isRunning = false;
-	        	while(true){
-	        		try{
-	        			ourThread.join();
-	        			
-	        		}catch(InterruptedException e){
-	        			e.printStackTrace();
-	        		}
-	        		break;
-	        	}
-	        	ourThread = null;
-	        }
-	
-	        public void resume(){
-	            isRunning = true;
-	            ourThread = new Thread(this);
-	            ourThread.start();
-	        }
-	        
-	        
-	        /**
-	         * Function to change Background picture
-	         * @param background id of new background drawable
-	         */
-	        public void changeBackground(int background){
-	        	this.background=background;
-	        }
-	        
-	        /**
-	         * Stops DrawShark thread
-	         */
-	        public void stop(){
-	        	isRunning=false;
-	        }
-	        private void drawShark(){
-	        	double delay = 0.4;  //sensitivity of sensor
-                Display display = getWindowManager().getDefaultDisplay();    // to get the Window height and width
-				@SuppressWarnings("deprecation") //deprecated function has to be used because of target android version
-				int width = display.getWidth();
-				@SuppressWarnings("deprecation") //deprecated function has to be used because of target android version
-				int height = display.getHeight();
-                
-                mBitmap = BitmapFactory.decodeResource(getResources(), background); //new Bitmap to make the background image
-                int h = width;
-                int w = height;
-                
-                Bitmap scaled = Bitmap.createScaledBitmap(mBitmap, h, w, true );
-                
-                Canvas canvas = ourHolder.lockCanvas(); 
-                canvas.drawColor(Color.WHITE);
-                canvas.drawBitmap(scaled, 0,0, new Paint()); // set Background
-                float startX = 350;
-                float startY = 150;
-                float offsetX = 0; 
-                float offsetY = 0;
-                //ignore tilt between ]-delay;delay[
-                if(sensorX>delay){  //if sensor value x is bigger than delay start moving picture
-                	offsetX=(float) ((100.0/360.0*(sensorX-delay))*width/4); 
-                }else if (sensorX<delay*-1){  //if sensor value x is smaller than -delay start moving picture
-                	offsetX=(float) ((100.0/360.0*(sensorX+delay))*width/4);
-                }
-                if(sensorY>delay){ //if sensor value y is bigger than delay start moving picture
-                	offsetY=(float) ((100.0/360.0*(sensorY-delay))*width/4);
-                }else if (sensorY<delay*-1){ //if sensor value y is smaller than -delay start moving picture
-                	offsetY=(float) ((100.0/360.0*(sensorY+delay))*width/4);
-                }
-                canvas.drawBitmap(shark, startX +offsetX, startY+offsetY, null);
-                ourHolder.unlockCanvasAndPost(canvas);
-	        }
-	        
-	        @Override
-	        public void run() {
-	            while (isRunning){
-	            	if (!ourHolder.getSurface().isValid()){
-	            		continue;
-	            	}
-	               drawShark();
-	            }
-	        }
+		SurfaceHolder ourHolder;
+		Thread ourThread = null;
+		boolean isRunning = true;
+		int background = R.drawable.ic_sky;
 
-	   }
+		public DrawShark(Context context) {
+			super(context);
+			ourHolder = getHolder();
+		}
 
-	    @Override
-	    protected void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE); // sensor listener to get the move of the device 
-	        if (sm.getSensorList(Sensor.TYPE_ACCELEROMETER).size() != 0){
-	            Sensor s = sm.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
-	            sm.registerListener(this, s, SensorManager.SENSOR_DELAY_NORMAL);
-	        }
-	        shark = BitmapFactory.decodeResource(getResources(), R.drawable.bellishark_1_medium);
-	        sensorX = sensorY = 0;
+		@Override
+		public void run() {
+			while (isRunning) {
+				if (!ourHolder.getSurface().isValid()) {
+					continue;
+				}
+				drawSharkImage();
+			}
+		}
 
-	        ourView = new DrawShark(this);
-	        ourView.resume();
-	        setContentView(ourView);
-	    }
-	    
-	    @Override
-	    public void onResume(){
-	    	ourView.resume();
-	    	super.onResume();
-	    }
+		// pauses DrawShark thread
+		public void pause() {
+			isRunning = false;
+			while (true) {
+				try {
+					ourThread.join();
 
-	    /**
-	     * Overrides changeBackground so bitmap is changed instead of background.
-	     */
-	    @Override
-	    public boolean onOptionsItemSelected(MenuItem item) {
-	    	if (item.getGroupId() == R.id.submenu_changeBackground) {
-	    		int id=0;
-	    		switch (item.getItemId()) {
-	    		 // set Background
-				case R.id.sky:// Picture sky is chosen as background
-					id=R.drawable.ic_sky;
-			       break;
-				case R.id.water:
-					id=R.drawable.sea;
-					break;
-				case R.id.th_picture:
-					id=R.drawable.th_ingolstadt;
-					break;
-				default: 
-					return false;
-			}		
-					ourView.changeBackground(id);
-			        return true;
-	    	}else if(item.getGroupId()==R.id.submenu_changeMode||item.getGroupId()==R.id.submenu_changeMove){
-	    		ourView.stop(); //stops drawing thread before mode is changed	
-	    	}
-	    	return super.onOptionsItemSelected(item);
-	    }
-	    
-	   
-	    /**
-	     * Stops drawing thread before execute step back
-	     */
-	    @Override
-	    public boolean onKeyDown(int keyCode, KeyEvent event) {
-	    	if(keyCode==KeyEvent.KEYCODE_BACK){
-	    		ourView.stop();
-	    	}
-	    	return super.onKeyDown(keyCode, event);
-	    }
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				break;
+			}
+			ourThread = null;
+		}
 
-	    
-	 // method for movement of the fish
-	    public abstract void move(int xAxis, int yAxis);
-	    // TODO: implement movement
-	    
+		// resumes DrawShark thread
+		public void resume() {
+			isRunning = true;
+			ourThread = new Thread(this);
+			ourThread.start();
+		}
+
+		// Stops DrawShark thread
+		public void stop() {
+			isRunning = false;
+		}
+
+		/**
+		 * Function to change Background picture
+		 * 
+		 * @param background
+		 *            id of new background drawable
+		 */
+		public void changeBackground(int background) {
+			this.background = background;
+		}
+
+		private void drawSharkImage() {
+			double delay = 0.4; // sensitivity of sensor
+			Display display = getWindowManager().getDefaultDisplay(); // to get the Window height and width
+			@SuppressWarnings("deprecation") // deprecated function has to be used because of target android version
+			int width = display.getWidth();
+			@SuppressWarnings("deprecation") // deprecated function has to be used because of target android version
+			int height = display.getHeight();
+
+			mBitmap = BitmapFactory.decodeResource(getResources(), background); // new Bitmap to make the background image
+
+			Bitmap scaled = Bitmap.createScaledBitmap(mBitmap, width, height, true);
+
+			Canvas canvas = ourHolder.lockCanvas();
+			canvas.drawColor(Color.WHITE);
+			canvas.drawBitmap(scaled, 0, 0, new Paint()); // set Background
+			
+			//calculate new position of shark
+			float startX = 350;
+			float startY = 150;
+			float offsetX = 0;
+			float offsetY = 0;
+			// ignore tilt between ]-delay;delay[
+			if (sensorX > delay) { // if sensor value x is bigger than delay
+									// start moving picture
+				offsetX = (float) ((100.0 / 360.0 * (sensorX - delay)) * width / 4);
+			} else if (sensorX < delay * -1) { // if sensor value x is smaller
+												// than -delay start moving
+												// picture
+				offsetX = (float) ((100.0 / 360.0 * (sensorX + delay)) * width / 4);
+			}
+			if (sensorY > delay) { // if sensor value y is bigger than delay
+									// start moving picture
+				offsetY = (float) ((100.0 / 360.0 * (sensorY - delay)) * width / 4);
+			} else if (sensorY < delay * -1) { // if sensor value y is smaller
+												// than -delay start moving
+												// picture
+				offsetY = (float) ((100.0 / 360.0 * (sensorY + delay)) * width / 4);
+			}
+			
+			canvas.drawBitmap(shark, startX + offsetX, startY + offsetY, null);
+			ourHolder.unlockCanvasAndPost(canvas);
+		}
+
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE); // sensor listener to get the move of the device
+		if (sm.getSensorList(Sensor.TYPE_ACCELEROMETER).size() != 0) {
+			Sensor s = sm.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
+			sm.registerListener(this, s, SensorManager.SENSOR_DELAY_NORMAL);
+		}
+		shark = BitmapFactory.decodeResource(getResources(),
+				R.drawable.bellishark_1_medium);
+		sensorX = sensorY = 0;
+
+		ourView = new DrawShark(this);
+		ourView.resume();
+		setContentView(ourView);
+	}
+
+	@Override
+	public void onResume() {
+		ourView.resume();
+		super.onResume();
+	}
+
+	/**
+	 * Overrides changeBackground so bitmap is changed instead of background. Stops thread if menu changes activity.
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getGroupId() == R.id.submenu_changeBackground) {
+			int id = 0;
+			switch (item.getItemId()) {
+			// set Background
+			case R.id.sky:// Picture sky is chosen as background
+				id = R.drawable.ic_sky;
+				break;
+			case R.id.water:
+				id = R.drawable.sea;
+				break;
+			case R.id.th_picture:
+				id = R.drawable.th_ingolstadt;
+				break;
+			default:
+				return false;
+			}
+			ourView.changeBackground(id);
+			return true;
+		} else if (item.getGroupId() == R.id.submenu_changeMode
+				|| item.getGroupId() == R.id.submenu_changeMove) {
+			ourView.stop(); // stops drawing thread before mode is changed
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * Stops drawing thread before execute step back
+	 */
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			ourView.stop();
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	// method for movement of the fish
+	public abstract void move(int xAxis, int yAxis);
+	// TODO: implement movement
+
 }
