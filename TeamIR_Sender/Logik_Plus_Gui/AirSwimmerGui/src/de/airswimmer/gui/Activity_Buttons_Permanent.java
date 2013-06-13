02f7bd02bb84a2825351a95ccc986a006b0e5bb2
Caroline Pilot <ff0425@haw-ingolstadt>
2013-06-13 17:32:28 +0200

    Permanente Tastensteuerung eingefügt Version 1.1

--- TeamIR_Sender/Logik_Plus_Gui/AirSwimmerGui/src/de/airswimmer/gui/Activity_Buttons_Permanent.java
+++ TeamIR_Sender/Logik_Plus_Gui/AirSwimmerGui/src/de/airswimmer/gui/Activity_Buttons_Permanent.java
@@ -1,49 +1,54 @@
 package de.airswimmer.gui;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class Activity_Buttons_Permanent extends BaseButtons {
 
-	private boolean forwardMovement = false;
+	private boolean forwardMovement = true;
+	private Button buttonStart;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		setContentView(R.layout.activity_layout_buttons_permanent);
 		super.onCreate(savedInstanceState);
+		buttonStart = (Button) findViewById(R.id.buttonStart);
+		buttonStart.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				
+				buttonStart.setText("stop");
+				while(forwardMovement){
+					swim();
+				}
+				buttonStart.setText("start");
+				
+			}
+		});
 	}
 
-	public void changeMoveState(View view) throws InterruptedException {
-		Button buttonstart = (Button) view;
-
+	public boolean swim() {
+		
 		if (forwardMovement) {
-
 			forwardMovement = false;
-			buttonstart.setText("Start");
-			permanent_thread.wait(10000);
-			Toast.makeText(view.getContext(), "stop", Toast.LENGTH_SHORT)
-					.show();
 
 		} else {
-			buttonstart.setText("Stop");
+			
 			forwardMovement = true;
+			moveLeft();
+			action.finishMovingLeft();
+			SystemClock.sleep(waiting_time);
+			moveRight();
+			action.finishMovingRight();
 			
 			
-			permanent_thread.run();
-
-			/*
-			 * while(forwardMovement){ moveRight();
-			 * Toast.makeText(view.getContext(), "move right",
-			 * Toast.LENGTH_SHORT) .show(); SystemClock.sleep(waiting_time);
-			 * moveLeft(); Toast.makeText(view.getContext(), "move left",
-			 * Toast.LENGTH_SHORT) .show(); i++; if(i==8) {
-			 * forwardMovement=false; }
-			 * 
-			 * }
-			 */
 		}
+		return false;
 
 	}
 
@@ -74,8 +79,7 @@ public class Activity_Buttons_Permanent extends BaseButtons {
 	private Runnable permanent_thread = new Runnable() {
 
 		public void run() {
-	
-			
+
 			// loop of right and left moves
 			do {
 				moveRight();
@@ -88,10 +92,12 @@ public class Activity_Buttons_Permanent extends BaseButtons {
 							.println("Exception while swimming forward after right move: "
 									+ e);
 				}
+
 				moveLeft();
-				
+
 				try {
-					// Thread should wait until AirSwimmer has moved to the left,
+					// Thread should wait until AirSwimmer has moved to the
+					// left,
 					// once
 					Thread.sleep(waiting_time);
 				} catch (InterruptedException e) {
@@ -99,12 +105,11 @@ public class Activity_Buttons_Permanent extends BaseButtons {
 							.println("Exception while swimming forward after left move: "
 									+ e);
 				}
+				action.finishMovingLeft();
+			} while (forwardMovement == true);
 
-			}while(forwardMovement == false);
-			
 		}
-		
-		
+
 	};
 
 }
