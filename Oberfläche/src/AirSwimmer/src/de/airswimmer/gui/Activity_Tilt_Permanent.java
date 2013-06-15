009e81c6fb0756ea8e2c8c1d4f7a2567f39b2dbf
Belguezar Kocak <ff0300@haw-ingolstadt.de>
2013-06-15 14:29:50 +0200

    oberfläche kippen movement

--- Oberfläche/src/AirSwimmer/src/de/airswimmer/gui/Activity_Tilt_Permanent.java
+++ Oberfläche/src/AirSwimmer/src/de/airswimmer/gui/Activity_Tilt_Permanent.java
@@ -27,4 +27,16 @@ public class Activity_Tilt_Permanent extends BaseTilt {
 	public void move(int xAxis, int yAxis) {
 		// TODO: implement movement
 	}
+
+	@Override
+	public void moveLeft() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveRight() {
+		// TODO Auto-generated method stub
+		
+	}
 }
