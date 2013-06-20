bbdf199b0c12a2aa6d33fb740e2b76c755e84686
Anja Hafner <ff0359@haw-ingolstadt.de>
2013-06-20 23:46:05 +0200

    SetSound SeekBar nicht manuell änderbar gemacht und nähere Beschreibung hinzugefügt

--- finaleApp/AirSwimmer/src/de/airswimmer/gui/SetSoundActivity.java
+++ finaleApp/AirSwimmer/src/de/airswimmer/gui/SetSoundActivity.java
@@ -16,176 +16,141 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class SetSoundActivity extends BaseActivity implements OnClickListener {
 
-	private Button okbutton;
-	private SeekBar bar;
-	private int value;
-	private boolean stopped = false;
-	private SharedPreferences prefs;
-	private Editor editor;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		prefs =  getSharedPreferences("AirSwimmerPrefs", Context.MODE_PRIVATE);
-		editor = prefs.edit();
-		editor.putInt("voice", 0);
-		editor.commit();
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_set_sound);
-		final SharedPreferences prefs = getSharedPreferences("AirSwimmerPrefs",
-				Context.MODE_PRIVATE); // get preferences which are stored in
-										// file AirSwimmerPrefs
-		int layout = prefs.getInt("layout", -1);
-		if (layout != -1) {
-			setRequestedOrientation(layout); // set screen orientation of
-												// current activity to stored
-												// value
-		}
-		okbutton = (Button) findViewById(R.id.OKButton); // set button
-		okbutton.setOnClickListener(this); // set listener
-		okbutton.performClick(); // calls onClickListener
+    private Button okbutton;
+    private SeekBar bar;
+    private int value;
+    private boolean stopped = false;
+    private SharedPreferences prefs;
+    private Editor editor;
+    protected FrontPage frontPage;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        prefs =  getSharedPreferences("AirSwimmerPrefs", Context.MODE_PRIVATE);
+        editor = prefs.edit();
+        editor.putInt("voice", 0);
+        editor.commit();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_sound);
+        final SharedPreferences prefs = getSharedPreferences("AirSwimmerPrefs",Context.MODE_PRIVATE); // get preferences which are stored in file AirSwimmerPrefs
+        int layout = prefs.getInt("layout", -1);
+        
+        if (layout != -1) {
+            setRequestedOrientation(layout); // set screen orientation of current activity to stored value
+        }
+        
+        okbutton = (Button) findViewById(R.id.OKButton); // set button
+        okbutton.setOnClickListener(this); // set listener
+        okbutton.performClick(); // calls onClickListener
 
-		bar = (SeekBar) findViewById(R.id.SoundBar); // set SeekBar
+        bar = (SeekBar) findViewById(R.id.SoundBar); // set SeekBar
+        bar.setEnabled(false);            //prevent user to change bar progress
 
-		bar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() // set and
-																		// create
-																		// OnSeekBarChangeListener
-		{
+        bar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() // set and create OnSeekBarChangeListener
+        {
 
-			public void onProgressChanged(SeekBar seekBar, int progress,
-					boolean fromUser) { // Notification that value from SeekBar
-										// has changed
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) { // Notification that value from SeekBar has changed
 
-				value = seekBar.getProgress(); // save progress
-				editor.putInt("voice", value);
-				editor.commit();
-				
-			}
+                value = seekBar.getProgress(); // save progress
+                editor.putInt("voice", value);
+                editor.commit();
+                
+            }
 
-			@Override
-			public void onStartTrackingTouch(SeekBar seekBar) { // Notification
-																// that user
-																// started touch
-																// gesture
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { // Notification that user started touch gesture
 
-			}
+            }
 
-			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) { // Notification
-																// that user
-																// stopped touch
-																// gesture
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { // Notification that user stopped touch gesture
 
-			}
-		});
+            }
+        });
 
-	}
+    }
 
-	protected void onStart() {
+    protected void onStart() {
 
-		super.onStart();
-		bar.setProgress(0);
+        super.onStart();
+        bar.setProgress(0);
 
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
 
-				try {
-					Thread.sleep(5000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
-				bar.setProgress(1);
+                bar.setProgress(1);
 
-				for (int i = 1; i < 16; i++) {
+                for (int i = 1; i < 16; i++) {
 
-					if (stopped == true) {
-						break;
-					}
+                    if (stopped == true) {
+                        break;
+                    }
 
-					bar.setProgress(i);
-					// comand send signal move_fish
-					for (int j = 0; j < 5; j++) {
-						action.diving();
-						
-						try {
-							Thread.sleep(400);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
+                    bar.setProgress(i);
+                    // comand send signal move_fish
+                    for (int j = 0; j < 5; j++) {
+                        action.diving();
+                        
+                        try {
+                            Thread.sleep(400);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
 
-					value = i;
-				}
+                    value = i;
+                }
 
-				runOnUiThread(new Runnable() {
-					@Override
-					public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
 
-					}
-				});
-			}
-		}).start();
+                    }
+                });
+            }
+        }).start();
 
-	}
+    }
 
-	@Override
-	public void onClick(View v) {
+    @Override
+    public void onClick(View v) {
 
-		if (v == okbutton) {
-			okbutton.setOnTouchListener(new OnTouchListener() { // create
-				// OnClickListener
-				// for okbutton
+        if (v == okbutton) {
+            okbutton.setOnTouchListener(new OnTouchListener() { // create OnClickListener for okbutton
 
-				@Override
-				public boolean onTouch(View v, MotionEvent event) { // called
-																	// when
-																	// screen is
-																	// touched
+                @Override
+                public boolean onTouch(View v, MotionEvent event) { // called when screen is touched
 
-					if (event.getAction() == MotionEvent.ACTION_DOWN) { // if
-																		// button
-																		// is
-																		// pressed
-						stopped = true;
-						okbutton.setBackgroundColor(Color.GRAY); // change
-																	// button
-																	// colour
-																	// (so you
-																	// can see
-																	// that
-																	// button
-																	// has been
-																	// pushed)
-						value = bar.getProgress();
-						AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-						audioManager.setStreamVolume(
-								AudioManager.STREAM_SYSTEM, value, 0);
+                    if (event.getAction() == MotionEvent.ACTION_DOWN) { // if button is pressed
+                        stopped = true;
+                        okbutton.setBackgroundColor(Color.GRAY); // change button colour (so you can see that button has been pushed)
+                        value = bar.getProgress();
+                        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                        audioManager.setStreamVolume(AudioManager.STREAM_SYSTEM, value, 0);
 
-						editor.putInt("voice", value);
-						editor.commit();
-						return true;
-					} else if (event.getAction() == MotionEvent.ACTION_UP) { // if
-																				// button
-																				// is
-																				// released
-						okbutton.setBackgroundColor(Color.LTGRAY); // change
-																	// button
-																	// colour
-																	// again (so
-																	// you can
-																	// see that
-																	// button
-																	// has been
-																	// released)
-						return true;
-					}
+                        editor.putInt("voice", value);
+                        editor.commit();
+                        return true;
+                        
+                    } 
+                    else if (event.getAction() == MotionEvent.ACTION_UP) { // if button is released
+                        okbutton.setBackgroundColor(Color.LTGRAY); // change button  colour again (so you can see that button has been released)
+                        return true;
+                    }
 
-					return false;
-				}
+                    return false;
+                }
 
-			});
-		}
+            });
+        }
 
-	}
+    }
 
 }
